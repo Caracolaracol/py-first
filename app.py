f972f929ea8e12ac3e6cf089bd20fc675b5512b6
Agustin Rojas <agus.rojasmolina@gmail.com>
2022-08-07 21:08:31 -0400

    boolean

--- app.py
+++ app.py
@@ -1,2 +1,7 @@
 age = 20
+# con punto se escriben los deciamles
+price = 19.95
+# string value, # es buena practica usar guion bajo
+first_name = "Mosh"
+is_online = False # boolean value
 print(age)
