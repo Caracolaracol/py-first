f8290039ee257b9158778a70325fbaa8097c7d9e
Agustin Rojas <agus.rojasmolina@gmail.com>
2022-08-07 21:16:32 -0400

    int

--- app.py
+++ app.py
@@ -14,3 +14,17 @@ new_patient = True
 
 name = input("What is your name?")
 print("Hello " + name) #string concatenation
+
+#type conversion 
+birth_year = input("Enter your birth year: ")
+# age = 2020 - birth_year # python no sabe como restar un string y number. para corregir eso hay que usar la funcion int 
+# print(age)
+
+age = 2020 - int(birth_year)
+print(age)
+
+# conversores:
+int()
+float()
+bool()
+str()
