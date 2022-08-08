9392fc400f062052866e99af697818df6ba730d6
Agustin Rojas <agus.rojasmolina@gmail.com>
2022-08-08 16:21:13 -0400

    for

--- app.py
+++ app.py
@@ -78,3 +78,34 @@ elif frase == 'hola':
     print('nos estan saludando')
 else:
     print('Esto es algo cuando no da ningun resultado')
+
+
+## ejercicio
+print('Elige tu propio camino')
+inicio = input("Escribe empezar para iniciar el programa: ")
+while (inicio == 'empezar'):
+    print(""" ¿Que camino quieres elegir?
+    escribe la opcion con numero
+    1 - Quiero que me saludes
+    2 - Deseo multiplicar ya que no se como hacerlo
+    3 - quiero salir de este programa, ya que aprendi a multiplicar con el curso de Alvaro """)
+    opcion = input()
+    if opcion == '1':
+        print("Te saludo")
+    elif opcion == '2':
+        numero1 = float(input('introduce el valor a multiplicar primero: '))
+        numero2 = float(input('introduce el valor a multiplicar segundo: '))
+        print('El resultado es: ', numero1*numero2)
+    elif opcion == '3':
+        print('Excelente decision que hayas tomado el curso de alvaro')
+        break
+    else:
+        print('No se que elegiste, tuviste que haber puesto algun numero de las opciones, se nota que no tomaste el curso de alvaro')
+
+## for
+
+lista = [1,2,3,4,5,6,7,8,9]
+indice = 0
+while indice < len(lista):
+    print(lista[indice])
+    indice+=1 
